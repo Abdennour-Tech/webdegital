ec625edea85ce81e05803949537069fd19cb22b1
Abdennour-Tech <baderabdennour123@gmail.com>
2026-08-10 03:28:45 +0100

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-DObzmQLc.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/HP/Desktop/webdegital/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-gkQBVcpU.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-gkQBVcpU.js" } }] }, "/": { filePath: "C:/Users/HP/Desktop/webdegital/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BIf0GO8R.js"] } } });
+export {
+  tsrStartManifest
+};
